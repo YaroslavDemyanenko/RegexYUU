fa7f0d4fc7b57107085dfeb7c60fcd50eb53b132
YaroslavDemyanenko <yaroslav.demyanenko@elorrieta-errekamari.com>
2019-12-16 01:00:27 +0100

    regex finalizado

--- src/main/Lanzador.java
+++ src/main/Lanzador.java
@@ -7,43 +7,16 @@ public class Lanzador {
 	public static void main(String[] args) {
 		RegexPropio reg=new RegexPropio();
 		
-		System.out.println(reg.match("yyyyy23u", "[&l]{1,}[123][13][&l]"));
+		
+		System.out.println(reg.match("yyyyy23u", "[y]{5}[&n]{1,2}[&l]{1,}"));
+		
+		
+		
 		System.out.println(reg.match("y27u", "[&l][123][13][&l]"));
 		System.out.println(reg.match("127u", "[&l][123][13][&l]"));
 		System.out.println(reg.match("y278", "[&l][123][13][&l]"));
 		System.out.println(reg.match("Y3u7", "[&l][&n][&l][&n]"));
 		
-		/**
-		System.out.println();
-		
-		System.out.println(reg.match("1234bsc", "&l&n&s"));
-		System.out.println(reg.match("1234bsc ", "&l&n&s"));
-		System.out.println(reg.match("1234bsc	", "&l&n&s"));
-		System.out.println(reg.match("1234bsc\n", "&l&n&s"));
-		
-		
-		
-		
-		
-		
-		//[1] 	 split("[")    &n[123][13]  &n,
-		
-		//123],13]  split("]")  123,13,""
-
-		System.out.println(reg.match("cxzc112323", "[1]"));
-		
-		System.out.println(reg.match("1234bsc", "&l&n")); //true
-		System.out.println(reg.match("1234", "&l&n"));	
-		System.out.println(reg.match("bsc", "&l&n"));
-		System.out.println("");
-		System.out.println(reg.match("bsc1234", "&n"));
-		System.out.println(reg.match("1234", "&n")); //true
-		System.out.println(reg.match("bsc", "&n"));
-		System.out.println("");
-		System.out.println(reg.match("1234bsc", "&l"));
-		System.out.println(reg.match("1234", "&l"));
-		System.out.println(reg.match("bsc", "&l")); //true
-		**/
 	}
 
 }
--- src/validador/RegexPropio.java
+++ src/validador/RegexPropio.java
@@ -7,9 +7,7 @@ public class RegexPropio {
 	private final char[] letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 	private final char[] numeros = "0123456789".toCharArray();
 	private final char[] espacios = " 	\n".toCharArray();
-	private ArrayList<String> reglasMetidas = null;
-
-	// ORDEN: L N S	
+	private ArrayList<String[]> reglasMetidas = null;
 
 	public boolean match(String cadena, String regex) {
 		reglasMetidas = sacarReglas(regex);
@@ -18,25 +16,34 @@ public class RegexPropio {
 		for (int x = 0; x < validaciones.length; x++) {
 			validaciones[x] = false;
 		}
-
-		if (cadena.length() == reglasMetidas.size()) {
+		try {
+			int posicionEnString = 0;
+			int longitudRegla = Integer.valueOf(reglasMetidas.get(0)[1].substring(2, 3));
 			for (int i = 0; i < reglasMetidas.size(); i++) {
-				switch (reglasMetidas.get(i)) {
+				switch (reglasMetidas.get(i)[0]) {
 				case "&l":
-					validaciones[i] = letrasL(cadena);
+					validaciones[i] = validarCharacteres(cadena.substring(posicionEnString, longitudRegla), reglasMetidas.get(i)[1], letras);
 					break;
 				case "&n":
-					validaciones[i] = numerosN(cadena);
+					validaciones[i] = validarCharacteres(cadena.substring(posicionEnString, longitudRegla), reglasMetidas.get(i)[1], numeros);
 					break;
 				case "&s":
-					validaciones[i] = espaciosS(cadena);
+					validaciones[i] = validarCharacteres(cadena.substring(posicionEnString, longitudRegla), reglasMetidas.get(i)[1], espacios);
 					break;
 				default:
-					validaciones[i]=caracteresEspecificos(cadena, i);
+					validaciones[i] = validarCharacteres(cadena.substring(posicionEnString, longitudRegla), reglasMetidas.get(i)[1], reglasMetidas.get(i)[0].toCharArray());
 					break;
 				}
+				if (i+1 != reglasMetidas.size()) {
+					posicionEnString = longitudRegla;
+					int aux = Integer.valueOf(reglasMetidas.get(i + 1)[1].substring(2, 3));
+					if (aux == 0) {
+						aux++;
+					}
+					longitudRegla = posicionEnString + aux;
+				}
 			}
-		} else {
+		} catch (ArrayIndexOutOfBoundsException e) {
 			return false;
 		}
 
@@ -48,55 +55,36 @@ public class RegexPropio {
 		return true;
 	}
 
-	private boolean caracteresEspecificos(String cadena, int i) {
-		for (int j = 0; j < reglasMetidas.get(i).length(); j++) {
-			if (reglasMetidas.get(i).charAt(j) == cadena.charAt(i)) {
-				return true;
-			}
+	private boolean validarCharacteres(String cadena, String taman, char[] tipo) {
+		boolean validacion = false;
+		int min = Integer.valueOf(taman.substring(0, 1));
+		int max = Integer.valueOf(taman.substring(2, 3));
+		if (max == 0) {
+			max = Integer.MAX_VALUE;
 		}
-		return false;
+
+		if (cadena.length() >= min && cadena.length() <= max) {
+			for (char caracter : cadena.toCharArray()) {
+				for (char esp : tipo) {
+					if (caracter == esp) {
+						validacion = true;
+						break;
+					}
+				}
+				if (!validacion) {
+					return false;
+				} else {
+					validacion = false;
+				}
+			}
+			return true;
+
+		} else {
+			return false;
+		}
 	}
 
-	private boolean espaciosS(String cadena) {
-		for (char espacio : espacios) {
-			if (cadena.contains("" + espacio)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private boolean letrasL(String cadena) {
-		for (char letr : letras) {
-			if (cadena.contains("" + letr)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public boolean numerosN(String cadena) {
-		for (char num : numeros) {
-			if (cadena.contains("" + num)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	//a-z A-Z
-
-	
-	
-	public ArrayList<String[]> sacarReglas2(String cadena) {
-		//						"[&l][123]{1,2}[13]"
-		//SPLIT("[") 			"" , "&l]" , "123]{1,2}" , "13]"
-		//SPLIT("]")			"" ERROR
-		//						"&l"
-		//						"123","{1,2}"
-		//						"13",""
-		
-		
+	public ArrayList<String[]> sacarReglas(String cadena) {
 		String[] primerSplit = { cadena };
 		ArrayList<String[]> reglas = new ArrayList<String[]>();
 		boolean primerSplitHecho = false;
@@ -108,73 +96,32 @@ public class RegexPropio {
 			for (String cadenaSpliteada : primerSplit) {
 				if (cadenaSpliteada.contains("]")) {
 					String[] segundoSplit = cadenaSpliteada.split("\\]");
-					
-					
-					for (String cadenaSpliteada2 : segundoSplit) {
-						if (!cadenaSpliteada2.equals("")) {
-							if (cadenaSpliteada2.contains("{")) {
-								
-								//						"123","{1,2}"
-								String[] cadenaSpliteada3=cadenaSpliteada2.split("{");
-								//{1,2} {1} {1,} {,3}
-								//  "1,2}" "1}" "1,}" ",2}"
-								if(cadenaSpliteada3[1].contains(",")) {
-									String[] cadenaSpliteada4=cadenaSpliteada3[1].split(",");
-									//"1","2}"  "1","}"  "","2}"
-									
-									
-									
-								}else {
-									reglas.add(new String[] {cadenaSpliteada2,cadenaSpliteada3[1].substring(0, 1)});
+					if (segundoSplit.length == 1) {
+						reglas.add(new String[] { segundoSplit[0], "1-1" });
+					} else if (!segundoSplit[1].equals("")) {
+						if (segundoSplit[1].contains("{")) {
+							String[] cadenaSpliteada3 = segundoSplit[1].split("\\{");
+							if (cadenaSpliteada3[1].contains(",")) {
+								String[] cadenaSpliteada4 = cadenaSpliteada3[1].split(",");
+								if (cadenaSpliteada4[0].equals("")) {
+									reglas.add(new String[] { segundoSplit[0], "0-" + cadenaSpliteada4[1].substring(0, cadenaSpliteada4[1].length() - 1) });
+								} else {
+									if (cadenaSpliteada4[1].equals("}")) {
+										reglas.add(new String[] { segundoSplit[0], cadenaSpliteada4[0] + "-0" });
+									} else {
+										reglas.add(new String[] { segundoSplit[0], cadenaSpliteada4[0] + "-" + cadenaSpliteada4[1].substring(0, cadenaSpliteada4[1].length() - 1) });
+									}
 								}
-								
-								
-								//reglas.add(new String[]cadenaSpliteada2.split(regex))
-							}else {
-								reglas.add(new String[]{cadenaSpliteada2,"{1}"});	
-							}													
+							} else {
+								reglas.add(new String[] { segundoSplit[0], cadenaSpliteada3[1].substring(0, cadenaSpliteada3[1].length() - 1) + "-" + cadenaSpliteada3[1].substring(0, cadenaSpliteada3[1].length() - 1) });
+							}
+						} else {
+							reglas.add(new String[] { segundoSplit[0], "1-1" });
 						}
 					}
-				} else {
+				} else if (!cadenaSpliteada.equals("")) {
 					if (!cadenaSpliteada.equals("")) {
-						reglas.add(new String[] {cadenaSpliteada,"{1}"});
-					}
-				}
-			}
-		}
-		return reglas;
-	}
-	
-	
-	
-	public ArrayList<String> sacarReglas(String cadena) {
-		//"[&l][123]{1,2}[13]"
-		//SPLIT("[") 			"" , "&l]" , "123]{1,2}" , "13]"
-		//SPLIT("]")			"" ERROR
-		//						"&l"
-		//						"123","{1,2}"
-		//						"13",""
-		
-		
-		String[] primerSplit = { cadena };
-		ArrayList<String> reglas = new ArrayList<String>();
-		boolean primerSplitHecho = false;
-		if (cadena.contains("[")) {
-			primerSplit = cadena.split("\\[");
-			primerSplitHecho = true;
-		}
-		if (cadena.contains("]") && primerSplitHecho) {
-			for (String cadenaSpliteada : primerSplit) {
-				if (cadenaSpliteada.contains("]")) {
-					String[] segundoSplit = cadenaSpliteada.split("\\]");
-					for (String cadenaSpliteada2 : segundoSplit) {
-						if (!cadenaSpliteada2.equals("")) {
-							reglas.add(cadenaSpliteada2);
-						}
-					}
-				} else {
-					if (!cadenaSpliteada.equals("")) {
-						reglas.add(cadenaSpliteada);
+						reglas.add(new String[] { cadenaSpliteada, "1-1" });
 					}
 				}
 			}
